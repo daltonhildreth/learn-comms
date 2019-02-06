9604fbb89fe798558145696b60a994dd1bd760f0
Dalton Hildreth <djdjhildreth@gmail.com>
2019-02-05 20:52:31 -0600

    Refactor pso_opt.py naming
    They were overly terse and hindered my understanding of the program

--- pso_opt.py
+++ pso_opt.py
@@ -1,18 +1,14 @@
 from random import uniform, random
 from shutil import copyfile
 from copy import copy
-from os import system, makedirs, getpid
+from os import mkdir, makedirs, getpid
+import subprocess
 from itertools import chain
 import argparse
 import numpy as np
 
 M_SHAPE = (4, 7)
 N_COMM = 2
-
-def call_dbg(prog, args):
-    sargs = [str(i) for i in args]
-    print(prog, " ".join(sargs))
-    system(prog + " " + " ".join(sargs))
 
 def write_config(file, config):
     file.write(str(M_SHAPE[0]) + " " + str(M_SHAPE[1]) + "\n")
@@ -28,96 +24,97 @@ def read_result(file):
     # will get results to influence nn choice from M
     avg_vel = float(file.readline().strip())
     confident_time = float(file.readline().strip())
-    return (avg_vel, confident_time)
+    return result_metric((avg_vel, confident_time))
 
 
 def result_metric(result):
     # this limits the results to just Julio's time confidence metric
-    _, conf_time = result
-    return conf_time
+    _, confident_time = result
+    return confident_time
 
 
 class Particle:
-    pid = 0
+    total_id = 0
 
-    def __init__(self, Blo, Bhi, scene):
-        self.pid_me = Particle.pid
-        Particle.pid += 1
+    def __init__(self, min_config, max_config, scene):
+        self.id = Particle.total_id
+        Particle.total_id += 1
 
-        self.Mx = np.zeros(shape=M_SHAPE)
-        for row in range(len(self.Mx)):
-            for col in range(len(self.Mx[row])):
+        spawn_config = np.zeros(shape=M_SHAPE)
+        for r in range(len(spawn_config)):
+            for c in range(len(spawn_config[r])):
                 # Don't fill in values that are redundant to TTC
-                if row > N_COMM - 1 and col > N_COMM - 1:
+                if r > N_COMM - 1 and c > N_COMM - 1:
                     continue
-                self.Mx[row][col] = uniform(Blo[row][col], Bhi[row][col])
-        self.Mx = Evaluation(self.Mx, scene)
+                spawn_config[r][c] = uniform(min_config[r][c], max_config[r][c])
+        self.pos = Evaluation(spawn_config, scene)
 
         # intentionally invalid, will be replaced at next minimize with self.Mx
-        self.Mp = Evaluation(None, None, float('inf'))
+        self.local_min = Evaluation(None, None, float('inf'))
 
-        self.Mv = np.zeros(shape=M_SHAPE)
-        for row in range(len(self.Mv)):
-            for col in range(len(self.Mv[row])):
+        self.vel = np.zeros(shape=M_SHAPE)
+        for r in range(len(self.vel)):
+            for c in range(len(self.vel[r])):
                 # Don't fill in values that are redundant to TTC
-                if row > N_COMM - 1 and col > N_COMM - 1:
+                if r > N_COMM - 1 and c > N_COMM - 1:
                     continue
-                mag = abs(Bhi[row][col] - Blo[row][col])
-                self.Mv[row][col] = uniform(-mag, mag)
+                mag = abs(max_config[r][c] - min_config[r][c])
+                self.vel[r][c] = uniform(-mag, mag)
 
-    def debug(self, base, Mg):
-        print("\t\ti\n", Evaluation.i)
-        print("\t\tpar_id\n", self.pid_me)
-        print("\t\tpid\n", getpid())
-        print("result\n", self.Mx.result)
-        print("r/b norm\n", self.Mx.result / base)
-        print("Gr\n", Mg.result)
-        print("r/Gr\n", self.Mx.result / Mg.result)
-        print("M\n", self.Mx.M)
-        print("V\n", self.Mv)
-        print("G\n", Mg.M)
+    def debug(self, base, global_min):
+        print("\t\ti ", Evaluation.i)
+        print("\t\tparticle id ", self.id)
+        print("\t\tprocess id ", getpid())
+        print("xr ", self.pos.result)
+        print("norm xr/b ", self.pos.result / base)
+        print("Gr ", global_min.result)
+        print("norm xr/Gr ", self.pos.result / global_min.result)
+        print("xM\n", self.pos.config)
+        print("vM\n", self.vel)
+        print("gM\n", global_min.config)
 
-    def gravitate(self, Mg, w_i, w_l, w_g):
-        for row in range(M_SHAPE[0]):
-            for col in range(M_SHAPE[1]):
-                if row > N_COMM - 1 and col > N_COMM - 1:
+    def gravitate(self, global_min, meta_args):
+        for r in range(M_SHAPE[0]):
+            for c in range(M_SHAPE[1]):
+                if r > N_COMM - 1 and c > N_COMM - 1:
                     continue
-                self.integrate_vel(row, col, Mg, w_i, w_l, w_g)
+                self.integrate_vel(r, c, global_min, meta_args)
 
-    def integrate_vel(self, r, c, Mg, w_i, w_l, w_g):
+    def integrate_vel(self, r, c, global_min, meta_args):
         local_flux = random()
         global_flux = random()
 
-        vel = self.Mv[r][c]
-        local_min = self.Mp.M[r][c]
-        global_min = Mg.M[r][c]
-        pos = self.Mx.M[r][c]
+        vel = self.vel[r][c]
+        local_min = self.local_min.config[r][c]
+        global_min = global_min.config[r][c]
+        pos = self.pos.config[r][c]
 
-        momentum = w_i * vel
-        local_gravity = w_l * local_flux * (local_min - pos)
-        global_gravity = w_g * global_flux * (global_min - pos)
+        momentum = meta_args.w_inertia * vel
+        local_gravity = meta_args.w_local * local_flux * (local_min - pos)
+        global_gravity = meta_args.w_global * global_flux * (global_min - pos)
 
-        self.Mv[r][c] = momentum + local_gravity + global_gravity
+        self.vel[r][c] = momentum + local_gravity + global_gravity
 
     def attempt_move(self, scene):
-        self.Mx = Evaluation(self.Mv + self.Mx.M, scene)
+        self.pos = Evaluation(self.vel + self.pos.config, scene)
 
-    def minimize(self, Mg):
-        if self.Mx < self.Mp:
-            self.Mp = self.Mx
-            if self.Mp < Mg:
-                Mg = self.Mp
-        return Mg
+    def minimize(self, global_min):
+        if self.pos < self.local_min:
+            self.local_min = self.pos
+            if self.local_min < global_min:
+                global_min = self.local_min
+        return global_min
 
 
 class Evaluation:
     i = 0
+    data_dir = ""
 
-    def __init__(self, M, scene, result=None):
-        self.M = M
+    def __init__(self, config, scene, result=None):
+        self.config = config
         self.scene = scene
         if result == None:
-            self.result = simulate(M, "t" + str(self.i), scene)
+            self.result = simulate(scene, config, self.data_dir, self.i)
             Evaluation.i += 1
         else:
             self.result = result
@@ -130,44 +127,58 @@ class Evaluation:
 
     def debug(self):
         print("result\n", self.result)
-        print("M\n", self.M)
+        print("config\n", self.config)
 
 class Scene:
     def __init__(self, id, seed):
         self.id = id
         self.seed = seed
 
-def simulate(M, trial, scene):
-    trial_path = str(scene.id) + "/" + trial
+class MetaParameters:
+    def __init__(self, n_particles, w_inertia, w_local, w_global):
+        self.n_particles = n_particles
+        self.w_inertia = w_inertia
+        self.w_local = w_local
+        self.w_global = w_global
 
-    with open('data/comms.config', 'w') as config_clone:
-        write_config(config_clone, M)
-    copyfile('data/comms.config', "data/" + trial_path + ".config")
+def simulate(scene, config, data_dir, save_path):
+    prog = "build/bin/comm_norender"
+    # the 0 does not matter since we are using a _norender build
+    args = [str(i) for i in [scene.id, scene.seed, 0, data_dir]]
+    data_dir = "data/" + data_dir
+    save = (data_dir, save_path)
 
-    call_dbg("build/bin/comm_norender", [scene.id, scene.seed])
+    sim_config = "%s/comms.config" % data_dir
+    with open(sim_config, 'w') as config_file:
+        write_config(config_file, config)
+    copyfile(sim_config, "%s/%s.config" % save)
 
-    copyfile('data/comms.result', 'data/' + trial_path + ".result")
-    with open('data/comms.result', 'r') as result_clone:
-        return result_metric(read_result(result_clone))
+    print(" ".join([prog] + args))
+    subprocess.call([prog] + args)
+
+    sim_result = "%s/comms.result" % data_dir
+    copyfile(sim_result, "%s/%s.result" % save)
+    with open(sim_result, 'r') as result_clone:
+        return read_result(result_clone)
 
 
-def PSO(scene, n, w_inertia=0.2, w_local=0.2, w_global=0.2):
+def PSO(scene, meta_args):
     particles = []  # [None] * n
     base_scene = copy(scene)
     #base_scene["with_comm"] = False
     base = Evaluation(np.zeros(shape=M_SHAPE), base_scene)
     base.debug()
-    Mg = Evaluation(np.empty(0), scene, result=float('inf'))
-    Blo = -np.ones(shape=M_SHAPE)
-    Bhi = np.ones(shape=M_SHAPE)
+    global_min = Evaluation(None, None, result=float('inf'))
+    min_config = -np.ones(shape=M_SHAPE)
+    max_config = np.ones(shape=M_SHAPE)
 
-    for p in range(n):
-        p = Particle(Blo, Bhi, scene)
-        Mg = p.minimize(Mg)
-        p.debug(base.result, Mg)
+    for p in range(meta_args.n_particles):
+        p = Particle(min_config, max_config, scene)
+        global_min = p.minimize(global_min)
+        p.debug(base.result, global_min)
         particles += [p]
 
-    conv_path = str(scene["id"]) + "/convergence.txt"
+    conv_path = Evaluation.data_dir + "/convergence.txt"
     with open("data/" + conv_path, 'w') as f_conv:
         f_conv.write("convergence value per full iteration of all particles\n")
  
@@ -176,27 +187,30 @@ def PSO(scene, n, w_inertia=0.2, w_local=0.2, w_global=0.2):
     for i in range(enough_iter):
         for p in particles:
             # integrate particle swarm dynamics
-            p.gravitate(Mg, w_inertia, w_local, w_global)
+            p.gravitate(global_min, meta_args)
             p.attempt_move(scene)
 
             # update known minima
-            Mg = p.minimize(Mg)
-            p.debug(base.result, Mg)
+            global_min = p.minimize(global_min)
+            p.debug(base.result, global_min)
 
         # the potential variation of the fastest particle
-        convergence = abs(max([np.linalg.norm(p.Mv) for p in particles]))
+        convergence = abs(max([np.linalg.norm(p.vel) for p in particles]))
         with open("data/" + conv_path, 'a') as f_conv:
             f_conv.write("i: " + str(i) + ",  c: " + str(convergence)+"\n")
         if convergence < enough_convergence:
             break
+        # YIELD HERE
+    # RETURN HERE
 
-    return particles
-
-def run_scenario(run_name, scene, meta_parameters):
-    makedirs("data/%s/%d_train/" % (run_name, scene["id"]))
-    makedirs("data/%s/results/%d/" % (run_name, scene["id"]))
-    PSO(scene, *meta_parameters)
+def run_scenario(run_name, scene, meta_args):
+    makedirs("data/%s/" % run_name)
+    mkdir("data/%s/%d_train/" % (run_name, scene.id))
+    makedirs("data/%s/results/%d/" % (run_name, scene.id))
+    Evaluation.data_dir = "%s/%d_train" % (run_name, scene.id)
+    PSO(scene, meta_args)
     Evaluation.i = 0
+    Particle.total_id = 0
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -209,14 +223,18 @@ if __name__ == '__main__':
     parser.add_argument("--w_local", type=float, default=0.2)
     parser.add_argument("--w_global", type=float, default=0.2)
     parser.add_argument("--seed", type=int, default=511607575)
-
     args = parser.parse_args()
 
     for scene in chain(range(0, 10 + 1), range(14, 17 + 1)):
         # HERE, FORK THIS:
         run_scenario(
-            args.run_name,
+            args.name,
             Scene(scene, args.seed),
-            [args.n_particles, args.w_inertia, args.w_local, args.w_global]
+            MetaParameters(
+                args.n_particles,
+                args.w_inertia,
+                args.w_local,
+                args.w_global
+            )
         )
     # JOIN HERE
