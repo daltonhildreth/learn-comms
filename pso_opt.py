1e876289de57a096ae2ca77a315177e578210748
Dalton Hildreth <djdjhildreth@gmail.com>
2019-02-07 20:30:01 -0600

    Reorganize data & scope dependencies
    Was previously very difficult to cross certain boundaries concurrently
    due to static variables and Constructor boundaries.

--- pso_opt.py
+++ pso_opt.py
@@ -2,7 +2,7 @@ from random import uniform, random
 from shutil import copyfile
 from copy import copy
 from os import mkdir, makedirs, getpid
-import subprocess
+from subprocess import Popen, PIPE
 from itertools import chain
 import argparse
 import numpy as np
@@ -34,11 +34,13 @@ def result_metric(result):
 
 
 class Particle:
-    total_id = 0
-
-    def __init__(self, min_config, max_config, scene):
-        self.id = Particle.total_id
-        Particle.total_id += 1
+    def __init__(self, id_, min_config, max_config, scene, data_dir):
+        self.id = id_
+        # the baseline always gets 0
+        self.iters = 1
+        self.scene = scene
+        self.data_dir = data_dir
+        self.save = "%d_p%d" % (self.iters, self.id)
 
         spawn_config = np.zeros(shape=M_SHAPE)
         for r in range(len(spawn_config)):
@@ -47,10 +49,10 @@ class Particle:
                 if r > N_COMM - 1 and c > N_COMM - 1:
                     continue
                 spawn_config[r][c] = uniform(min_config[r][c], max_config[r][c])
-        self.pos = Evaluation(spawn_config, scene)
+        self.pos = simulate(self.scene, spawn_config, self.data_dir, self.save)
 
-        # intentionally invalid, will be replaced at next minimize with self.Mx
-        self.local_min = Evaluation(None, None, float('inf'))
+        # intentionally invalid, will be replaced at next minimize with self.pos
+        self.local_min = Point()
 
         self.vel = np.zeros(shape=M_SHAPE)
         for r in range(len(self.vel)):
@@ -62,25 +64,25 @@ class Particle:
                 self.vel[r][c] = uniform(-mag, mag)
 
     def debug(self, base, global_min):
-        print("\t\ti ", Evaluation.i)
+        print("\t\ti ", self.iters)
         print("\t\tparticle id ", self.id)
         print("\t\tprocess id ", getpid())
-        print("xr ", self.pos.result)
-        print("norm xr/b ", self.pos.result / base)
-        print("Gr ", global_min.result)
-        print("norm xr/Gr ", self.pos.result / global_min.result)
+        print("xr ", self.pos.score)
+        print("norm xr/b ", self.pos.score / base.score)
+        print("Gr ", global_min.score)
+        print("norm xr/Gr ", self.pos.score / global_min.score)
         print("xM\n", self.pos.config)
         print("vM\n", self.vel)
         print("gM\n", global_min.config)
 
-    def gravitate(self, global_min, meta_args):
+    def gravitate(self, global_min, hypers):
         for r in range(M_SHAPE[0]):
             for c in range(M_SHAPE[1]):
                 if r > N_COMM - 1 and c > N_COMM - 1:
                     continue
-                self.integrate_vel(r, c, global_min, meta_args)
+                self.integrate_vel(r, c, global_min, hypers)
 
-    def integrate_vel(self, r, c, global_min, meta_args):
+    def integrate_vel(self, r, c, global_min, hypers):
         local_flux = random()
         global_flux = random()
 
@@ -89,57 +91,46 @@ class Particle:
         global_min = global_min.config[r][c]
         pos = self.pos.config[r][c]
 
-        momentum = meta_args.w_inertia * vel
-        local_gravity = meta_args.w_local * local_flux * (local_min - pos)
-        global_gravity = meta_args.w_global * global_flux * (global_min - pos)
+        momentum = hypers.w_inertia * vel
+        local_gravity = hypers.w_local * local_flux * (local_min - pos)
+        global_gravity = hypers.w_global * global_flux * (global_min - pos)
 
         self.vel[r][c] = momentum + local_gravity + global_gravity
 
-    def attempt_move(self, scene):
-        self.pos = Evaluation(self.vel + self.pos.config, scene)
+    def attempt_move(self):
+        self.iters += 1
+        self.pos = simulate(
+            self.scene, self.vel + self.pos.config, self.data_dir, self.save
+        )
 
     def minimize(self, global_min):
-        if self.pos < self.local_min:
+        if self.pos.score < self.local_min.score:
             self.local_min = self.pos
-            if self.local_min < global_min:
+            if self.local_min.score < global_min.score:
                 global_min = self.local_min
         return global_min
 
-
-class Evaluation:
-    i = 0
-    data_dir = ""
-
-    def __init__(self, config, scene, result=None):
-        self.config = config
-        self.scene = scene
-        if result == None:
-            self.result = simulate(scene, config, self.data_dir, self.i)
-            Evaluation.i += 1
-        else:
-            self.result = result
-
-    def __gt__(self, other):
-        return self.result > other.result
-
-    def __lt__(self, other):
-        return self.result < other.result
-
-    def debug(self):
-        print("result\n", self.result)
-        print("config\n", self.config)
+class Point:
+    def __init__(self, x=None, y=float('inf')):
+        self.config = x
+        self.score = y
 
 class Scene:
-    def __init__(self, id, seed):
-        self.id = id
+    def __init__(self, id_, seed):
+        self.id = id_
         self.seed = seed
 
-class MetaParameters:
-    def __init__(self, n_particles, w_inertia, w_local, w_global):
+class HyperParameters:
+    def __init__(
+        self, n_particles, w_inertia, w_local, w_global, max_iters,
+        max_convergence
+    ):
         self.n_particles = n_particles
         self.w_inertia = w_inertia
         self.w_local = w_local
         self.w_global = w_global
+        self.max_iters = max_iters
+        self.max_convergence = max_convergence
 
 def simulate(scene, config, data_dir, save_path):
     prog = "build/bin/comm_norender"
@@ -154,61 +145,58 @@ def simulate(scene, config, data_dir, save_path):
     copyfile(sim_config, "%s/%s.config" % save)
 
     print(" ".join([prog] + args))
-    subprocess.call([prog] + args)
+    p = Popen([prog] + args) # yield after this?
 
     sim_result = "%s/comms.result" % data_dir
     copyfile(sim_result, "%s/%s.result" % save)
     with open(sim_result, 'r') as result_clone:
-        return read_result(result_clone)
+        return Point(config, read_result(result_clone))
 
 
-def PSO(scene, meta_args):
-    particles = []  # [None] * n
-    base_scene = copy(scene)
-    #base_scene["with_comm"] = False
-    base = Evaluation(np.zeros(shape=M_SHAPE), base_scene)
-    base.debug()
-    global_min = Evaluation(None, None, result=float('inf'))
+def PSO(data_dir, scene, hypers):
+    base_scn = copy(scene)
+    baseline = simulate(base_scn, np.zeros(shape=M_SHAPE), data_dir, str(0))
+    # === SIMULATE BLOCKS HERE ===
+    print("baseline ==== ", baseline.score)
+    global_min = Point()
+
+    # initialize particles
     min_config = -np.ones(shape=M_SHAPE)
     max_config = np.ones(shape=M_SHAPE)
-
-    for p in range(meta_args.n_particles):
-        p = Particle(min_config, max_config, scene)
+    particles = []
+    for p in range(hypers.n_particles):
+        p = Particle(p, min_config, max_config, scene, data_dir)
+        # === SIMULATE BLOCKS HERE ===
         global_min = p.minimize(global_min)
-        p.debug(base.result, global_min)
+        p.debug(baseline, global_min)
         particles += [p]
 
-    conv_path = Evaluation.data_dir + "/convergence.txt"
+    conv_path = data_dir + "/convergence.txt"
     with open("data/" + conv_path, 'w') as f_conv:
         f_conv.write("convergence value per full iteration of all particles\n")
  
-    enough_iter = 100
-    enough_convergence = 1e-9
-    for i in range(enough_iter):
+    for i in range(hypers.max_iters):
         for p in particles:
             # integrate particle swarm dynamics
-            p.gravitate(global_min, meta_args)
-            p.attempt_move(scene)
+            p.gravitate(global_min, hypers)
+            p.attempt_move()
+            # === SIMULATE BLOCKS HERE ===
 
             # update known minima
             global_min = p.minimize(global_min)
-            p.debug(base.result, global_min)
+            p.debug(baseline, global_min)
 
         # the potential variation of the fastest particle
         convergence = abs(max([np.linalg.norm(p.vel) for p in particles]))
         with open("data/" + conv_path, 'a') as f_conv:
             f_conv.write("i: " + str(i) + ",  c: " + str(convergence)+"\n")
-        if convergence < enough_convergence:
-            break
-        # YIELD HERE
-    # RETURN HERE
+        if convergence < hypers.max_convergence:
+            return
 
 def run_scenario(run_name, scene, meta_args):
-    makedirs("data/%s/%d_train/" % (run_name, scene.id))
-    Evaluation.data_dir = "%s/%d_train" % (run_name, scene.id)
-    PSO(scene, meta_args)
-    Evaluation.i = 0
-    Particle.total_id = 0
+    data_dir = "%s/%d_train" % (run_name, scene.id)
+    makedirs("data/" + data_dir)
+    PSO(data_dir, scene, meta_args)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -221,18 +209,49 @@ if __name__ == '__main__':
     parser.add_argument("--w_local", type=float, default=0.2)
     parser.add_argument("--w_global", type=float, default=0.2)
     parser.add_argument("--seed", type=int, default=511607575)
+    parser.add_argument("--iters", type=int, default=100)
+    parser.add_argument("--convergence", type=float, default=1e-9)
     args = parser.parse_args()
 
+    # High-Level what should be going on.
+    # while optimizing:
+    #    for scene in scenes:
+    #        run(scene):
+    #            #...
+    #            next(PSO(scene)):
+    #                #...
+    #                R = yield from simulate(scene):
+    #                    #...
+    #                    o = Popen
+    #                    while o.poll() is not None:
+    #        /---------------yield None
+    #        \-------------->
+    #                ^       #...
+    #                \---yield r
+    #                while iterating:
+    #                    for p in particles:
+    #                        #...
+    #                        p.R = yield from simulate(scene):
+    #                            #...
+    #                            o = Popen
+    #                            while o.poll() is not None:
+    #        /-----------------------yield None
+    #        \---------------------->
+    #                        ^   #...
+    #                        \---yield r
+    #                    #...
+    #                #...
+    #            #...
     for scene in chain(range(0, 10 + 1), range(14, 17 + 1)):
-        # HERE, FORK THIS:
         run_scenario(
             args.name,
             Scene(scene, args.seed),
-            MetaParameters(
+            HyperParameters(
                 args.n_particles,
                 args.w_inertia,
                 args.w_local,
-                args.w_global
+                args.w_global,
+                args.iters,
+                args.convergence
             )
         )
-    # JOIN HERE
