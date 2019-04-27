063c4ad71f3449a2cefacdadc6cc6e757256bb3a
Dalton Hildreth <djdjhildreth@gmail.com>
2019-04-27 02:18:46 -0500

    Set non-linearity to softsign -1..1
    I ended up sticking with -1..1 as it is more versatile
    for later fundamental changes to trying to improve generalization.
    
    it's easy to switch to 0..1, though, just for a final test.
    
    Also, I chose softsign over atan because it seemed to have somewhat
    nicer visualizations, and it mathematically makes more sense if we're
    going for a sigmoidal function with slow saturations. It was basically
    made for that purpose!

--- enginee/src/comms/CommComp.h
+++ enginee/src/comms/CommComp.h
@@ -18,34 +18,12 @@ struct CommComp {
     void buf_in(comm::vecc c_prime, Eigen::Vector2f f_prime) {
         // distributed non-linearity
         for (int i = 0; i < NCOMM; ++i) {
-            // const float y = .5f;
-            // const float b = .5f;
-            const float x = c_prime[i]; // y
-            c_buf[i] =
-                // atan "wrong"
-                // glm::two_over_pi<float>() * std::atan(x);
-                // tanh
-                // std::tanh(x);
-                // Softsign (multiply denom x by 2 for 0..1)
-                // x / (1.f + std::fabs(2.f * x));
-                // atan "correct" (approximates y=x for small x)
-                // remove two's to make 0..1
-                // glm::one_over_pi<float>() * std::atan(glm::pi<float>() * x);
-                // ISRU (multiply denom x by 4 for 0..1)
-                x / std::sqrt(1.f + x * x);
-            // Soft.... ?
-            // 4.f * x / (4.f + x * x);
-            // clamp
-            // glm::clamp(x, -1.f, 1.f);
-            // sigmoid 0..1
-            // 1.f / (1.f + std::exp(-x));
-
-            // TODO: optimize p over
-            // Soft Lp-normalized
-            // x / std::pow(1.f + std::pow(std::fabs(x), p)), 1.f/p);
-
-            // c_buf[i] *= y;
-            // c_buf[i] += b;
+            const float x = c_prime[i];
+            c_buf[i] = x / (1.f + std::fabs(x));
+            // x / (1.f + std::fabs(2.f * x)) + 0.5f;
+            // glm::two_over_pi<float>() * std::atan(glm::half_pi<float>() * x);
+            // glm::one_over_pi<float>() * std::atan(glm::pi<float>() * x) +
+            // 0.5f;
         }
 
         // max force
